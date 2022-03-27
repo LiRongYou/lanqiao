2eb89063653bd945dd9df40e945fb929918661c7
LiRongYou <2302771663@qq.com>
2022-03-27 12:38:31 +0800

    修改了作业测试题的内容

--- src/main/java/作业题测试.java
+++ src/main/java/作业题测试.java
@@ -1,17 +1,6 @@
 
 public class 作业题测试{
     public static void main(String[] args) {
-        int a[] = { 0, 1, 2, 3, 4 };
-        int sum = 0;
-        try {
-            for (int i = 0; i < 6; i++) {
-                sum += a[i];
-            }
-            System.out.print(sum);
-        } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-            System.out.print(" 数组下标越界");
-        } finally {
-            System.out.print(" 程序结束");
-        }
+        System.out.println("github使用");
     }
 }
