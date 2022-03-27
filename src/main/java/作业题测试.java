441862b941ec9fcc657b5ccfaa7dac045165657b
“LiRongYou” <2302771663@qq.com>
2022-03-27 18:19:01 +0800

    删除了一行"有点懂了Github"代码

--- src/main/java/作业题测试.java
+++ src/main/java/作业题测试.java
@@ -3,6 +3,5 @@ public class 作业题测试{
     public static void main(String[] args) {
         System.out.println("github使用");
         System.out.println("欢迎");
-        System.out.println("有点懂了Github");
     }
 }
